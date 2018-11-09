1fd68978c202c15f5a6a1c3c42c3e0a72bd56e8e
15827311708 <410019994@qq.com>
2018-11-09 15:03:03 +0800

    test

---
+++ cosumer-client/src/main/java/com/tedu/RibbonRuleConfig.java
@@ -0,0 +1,15 @@
+package com.tedu;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.netflix.loadbalancer.IRule;
+import com.netflix.loadbalancer.RandomRule;
+
+@Configuration
+public class RibbonRuleConfig {
+	@Bean
+	public IRule ribbonRule() {
+		return new RandomRule();
+	}
+}
